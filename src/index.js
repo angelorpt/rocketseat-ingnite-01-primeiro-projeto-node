6a0e2100aebd49ffa648ef1a5b562989d3531411
Angelo Rafael <angelorpt@gmail.com>
2021-10-09 15:57:37 -0400

    add | Adicionando o Méoto para Salvar uma nova Conta

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+
 const app = express();
+
+const customers = [];
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (_, res) => {
   res.json({ hello: "world" });
+});
+
+/**
+ * cpf string
+ * name string
+ * id uuid
+ * statement []
+ */
+app.post("/accounts", (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidv4();
+  const customer = {
+    id,
+    cpf,
+    name,
+    statement: [],
+  };
+  customers.push(customer);
+
+  return res.status(201).send(customer);
 });
 
 app.listen(3000, () => console.log("API Running..."));
